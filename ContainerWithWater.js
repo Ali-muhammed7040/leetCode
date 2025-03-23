5f92f16b440f940ed50fa1c03c59d3a486d7798c
Ali Muhammad <81983704+Ali-muhammed7040@users.noreply.github.com>
2025-03-24 01:31:19 +0500

    container with most water

---
+++ ContainerWithWater.js
@@ -0,0 +1,13 @@
+let container = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+
+let maxWater = 0
+for (let i = 0; i < container.length; i++) {
+    for (let j = i + 1; j < container.length; j++) {
+        let minHeight = Math.min(container[i], container[j])
+        let width = j - i;
+        let currentWater = minHeight * width
+        maxWater = Math.max(currentWater, maxWater)
+    }
+}
+
+console.log(maxWater, 'maxWater')
